5c17c539ec20777d78c97cd85864bb6db2b5789d
mystar <eric_st@web.de>
2017-08-17 11:44:10 +0200

    resolved issue: CMake organization #1; made cuda arch detection portable; possibility of providing own cuda arch; updated docs

--- README.md
+++ README.md
@@ -3,17 +3,20 @@ Multicriterial cellular Evolutionary Algorithm
 
 ## Usage
 
-There is a GPU variant of the algorithm residing in `gpu/` and the CPU variant in `cpu/`.
+There is a GPU variant of the algorithm residing in `src/gpu/` and the CPU variant in `src/cpu/`.
 Test them via:
 
 ```
-cmake .
+mkdir -p build/release
+cd build/release
+cmake -DCMAKE_BUILD_TYPE=Release -DCUDA_ARCH=<insert arch number here (e.g.: 52)> ../..
 make
 ```
 
-... in the respetive directories.
+There is also a `-DCMAKE_BUILD_TYPE=Debug` build type available and if you omit the `-DCUDA_ARCH` cmake will try to find the cuda architecture itself (linux only).
 
-After that multiple variants with different parameters are located in `/gpu/bin/` or `cpu/bin`.
+
+After that multiple variants with different parameters are located in `build/release/gpu/` or `build.release/cpu/`.
 The used parameters are coded into the binary-name like this:
 
 ```
@@ -21,9 +24,8 @@ The used parameters are coded into the binary-name like this:
 ```
 
 You need a NVidia GPU and the CUDA framework for this obviously.
-Also you need cmake and make for compilation.
-All the DTLZ problems (1-7) can be optimized, but the mcea.<cu|cpp> has to be altered.
-Just change all occurrences of the dtlzX() function (X is the problem number) to your preferred variant.
+Also you need cmake (min 3.0.2) and make for compilation.
+All the DTLZ problems (1-7) can be optimized, just change the `DTLZ_NUM` parameter in the config files located in `src/cpu|gpu/config/`.
 Also a documentation how the algorithm works is coming.
 
-To create a doc from the source run `doxygen` in `gpu/` and `cpu/`.
+To create a doc from the source run `build_docs.sh` in `doc/`.
--- src/gpu/get_cuda_sm.sh
+++ src/gpu/get_cuda_sm.sh
@@ -6,13 +6,11 @@
 
 device_index=${1:-0}
 timestamp=$(date +%s.%N)
-gcc_binary=${CMAKE_CXX_COMPILER:-$(which c++)}
-cuda_root=${CUDA_DIR:-/usr/local/cuda}
-CUDA_INCLUDE_DIRS=${CUDA_INCLUDE_DIRS:-${cuda_root}/include}
-CUDA_CUDART_LIBRARY=${CUDA_CUDART_LIBRARY:-${cuda_root}/lib64/libcudart.so}
+nvcc_exec=$(which nvcc)
+generated_source="/tmp/cuda-compute-version-helper-$$-$timestamp.cu"
 generated_binary="/tmp/cuda-compute-version-helper-$$-$timestamp"
 # create a 'here document' that is code we compile and use to probe the card
-source_code="$(cat << EOF
+echo " 
 #include <stdio.h>
 #include <cuda_runtime_api.h>
 
@@ -23,26 +21,27 @@ int main()
 	int device_count;
 	status = cudaGetDeviceCount(&device_count);
 	if (status != cudaSuccess) {
-		fprintf(stderr,"cudaGetDeviceCount() failed: %s\n", cudaGetErrorString(status));
+		fprintf(stderr,\"cudaGetDeviceCount() failed: %s\n\", cudaGetErrorString(status));
 		return -1;
 	}
 	if (${device_index} >= device_count) {
-		fprintf(stderr, "Specified device index %d exceeds the maximum (the device count on this system is %d)\n", ${device_index}, device_count);
+		fprintf(stderr, \"Specified device index %d exceeds the maximum (the device count on this system is %d)\n\", ${device_index}, device_count);
 		return -1;
 	}
 	status = cudaGetDeviceProperties(&prop, ${device_index});
 	if (status != cudaSuccess) {
-		fprintf(stderr,"cudaGetDeviceProperties() for device ${device_index} failed: %s\n", cudaGetErrorString(status));
+		fprintf(stderr,\"cudaGetDeviceProperties() for device ${device_index} failed: %s\n\", cudaGetErrorString(status));
 		return -1;
 	}
 	int v = prop.major * 10 + prop.minor;
-	printf("%d", v);
-}
-EOF
-)"
-echo "$source_code" | $gcc_binary -x c++ -I"$CUDA_INCLUDE_DIRS" -o "$generated_binary" - -x none "$CUDA_CUDART_LIBRARY"
+	printf(\"%d\", v);
+}" > "$generated_source"
+
+$nvcc_exec -o $generated_binary $generated_source 2> /dev/null
+# echo "$source_code" | $gcc_binary -x c++ -I"$CUDA_INCLUDE_DIRS" -o "$generated_binary" - -x none "$CUDA_CUDART_LIBRARY"
 
 # probe the card and cleanup
 
 $generated_binary
+rm $generated_source
 rm $generated_binary
